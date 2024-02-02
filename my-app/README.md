a74b5ded7adc49d3ef5b6cbb20cf72d980f4ce2c
ISHA SUDHIR <ishasudhir26@gmail.com>
2024-02-02 11:32:27 +0530

    code here

--- my-app/README.md
+++ my-app/README.md
@@ -70,79 +70,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
 
 
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello how are you</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
 
 
 
-.App {
-  text-align: center;
-}
-.Appxtra {
-  text-align: left;
-  color: #000;
-}
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
 
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #173120;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: rgb(238, 205, 16);
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-create repository
-git init
-git add .
-if needed config using the given command
-git commit -am "some msg"
+*
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,166 +1,36 @@
-// Filename - App.js
- 
-import React from "react";
-import "./App.css";
- import image from './reactimg.jpg';
-function App() {
+import React from 'react';
+import './App.css';
+import image from './fortnite.jpg';
+import for1 from './fort1.jpg';
+import for2 from './fort2.jpg';
+import for3 from './fort3.jpg';
+function App(){
     return (
-        <div>
-            <nav class="navbar background">
-                <ul class="nav-list">
-                    <div class="logo">
-                        <img src=
-"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png"
-                        />
-                    </div>
-                    <li>
-                        <a href="#courses">Courses</a>
-                    </li>
-                    <li>
-                        <a href="#tutorials">Tutorials</a>
-                    </li>
-                    <li>
-                        <a href="#jobs">Jobs</a>
-                    </li>
-                    <li>
-                        <a href="#student">Student</a>
-                    </li>
-                    <li>
-                        <a href="https://www.geeksforgeeks.org/how-to-create-a-website-in-react-js/">React-learn</a>
-                    </li>
-                </ul>
- 
-                <div class="rightNav">
-                    <input
-                        type="text"
-                        name="search"
-                        id="search"
-                    />
-                    <button class="btn btn-sm">
-                        Search
-                    </button>
-                </div>
-            </nav>
- 
-            <section class="section">
-                <div class="box-main">
-                    <div class="firstHalf">
-                        <h1 class="text-big">
-                            7 Best Tips To Speed Up Your Job
-                            Search in 2022
-                        </h1>
-                        
-                        <img src={image} alt="test image"/>
-                        
-                        <p class="text-small">
-                            Hunting down a relevant job
-                            requires proper techniques for
-                            showcasing your potential to the
-                            employer. But with the advent of
-                            COVID-19, it has become a bit
-                            challenging and competitive to
-                            reach out for your dream job.
-                            Many individuals have lost their
-                            jobs during these times, and on
-                            the other hand, freshers are
-                            facing difficulties while
-                            applying for a new job. But
-                            there is no need for panic, you
-                            can change your ways and
-                            streamline things in a way that
-                            you get a proper result.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            JavaScript Tutorial
-                        </h1>
-                        <p class="text-small">
-                            JavaScript is the world most
-                            popular lightweight, interpreted
-                            compiled programming language.
-                            It is also known as scripting
-                            language for web pages. It is
-                            well-known for the development
-                            of web page many non-browser
-                            environments also use it.
-                            JavaScript can be used for
-                            Client-side developments as well
-                            as Server-side developments.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            Java Programming Language
-                        </h1>
-                        <p class="text-small">
-                            When compared with C++, Java
-                            codes are generally more
-                            maintainable because Java does
-                            not allow many things which may
-                            lead to bad/inefficient
-                            programming if used incorrectly.
-                            For example, non-primitives are
-                            always references in Java. So we
-                            cannot pass large objects (like
-                            we can do in C++) to functions,
-                            we always pass references in
-                            Java. One more example, since
-                            there are no pointers, bad
-                            memory access is also not
-                            possible. When compared with
-                            Python, Java kind of fits
-                            between C++ and Python. The
-                            programs are written in Java
-                            typically run faster than
-                            corresponding Python programs
-                            and slower than C++. Like C++,
-                            Java does static type checking,
-                            but Python does not.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            What is Machine Learning?
-                        </h1>
-                        <p class="text-small">
-                            Machine Learning is the field of
-                            study that gives computers the
-                            capability to learn without
-                            being explicitly programmed. ML
-                            is one of the most exciting
-                            technologies that one would have
-                            ever come across. As it is
-                            evident from the name, it gives
-                            the computer that makes it more
-                            similar to humans: The ability
-                            to learn. Machine learning is
-                            actively being used today,
-                            perhaps in many more places than
-                            one would expect.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <footer className="footer">
-                <p className="text-footer">
-                    Copyright ©-All rights are reserved
-                </p>
-            </footer>
-        </div>
-    );
+        <div className='back'>
+    <p className='textfirst'>Epic Games: An American Video Game and Software Developer And Publisher Based in Cary,North Carolina</p>
+    
+        <img src= {image} className="imagefirst" alt="picture"/>
+        
+        <p className='para'>Create play and battle with friends for free in Fortnite.Be the last player standing in battle Royale and Zero Build,experience live event, or discover over a million creator made games, including racing,parkour,zombie survival and more.Each Fortnite island has an individual age rating so you can find the one thats right for you and your friends.find it all in fortnite...Drop in.</p>
+     <div className='buttonatt'>
+         <button className='but'>Visit Website</button>
+     
+      </div>
+     
+      <div className='d-flex justify-content-start'>
+     <img src={for1} className="imagesec" alt="ok" />
+      
+     </div>
+     <div className='d-flex justify-content-center'>
+     
+     <img src={for2} className="imagesec" alt="picture2" />
+     </div>
+     <div className='d-flex justify-content-end'>
+     <img src={for3} className="imagesec" alt="pic3" />
+     </div>
+     
+      </div>
+    )
+    
 }
- 
 export default App;
